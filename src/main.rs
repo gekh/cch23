a34f0e3be519868c9863d80cedbd690c87e8a065
German Khokhlov <ouchlock@gmail.com>
2023-12-18 19:02:52 +0200

    13

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use log::info;
 use num_traits::PrimInt;
 use regex::Regex;
 use serde_json::{json, Value};
+use sqlx::PgPool;
 use std::{collections::HashMap, sync::Arc, time::SystemTime};
 use std::{io::Cursor, sync::Mutex};
 use tower_http::services::ServeDir;
@@ -348,11 +349,126 @@ async fn ulids_weekday(
     Ok(ulid_stats.into())
 }
 
+#[derive(Clone)]
+struct DbState {
+    pool: PgPool,
+}
+
+async fn sql(State(state): State<DbState>) -> Result<String, StatusCode> {
+    info!("13 sql started");
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(2023_12_13_i64)
+        .fetch_one(&state.pool)
+        .await
+        .map_err(|_err| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    Ok(row.0.to_string())
+}
+
+async fn reset(State(state): State<DbState>) -> Result<String, StatusCode> {
+    info!("13 reset started");
+
+    let sql = "DROP TABLE IF EXISTS orders";
+    sqlx::query(sql)
+        .execute(&state.pool)
+        .await
+        .map_err(|_err| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    let sql = "CREATE TABLE orders (
+            id INT PRIMARY KEY,
+            region_id INT,
+            gift_name VARCHAR(50),
+            quantity INT
+          )";
+    sqlx::query(sql)
+        .execute(&state.pool)
+        .await
+        .map_err(|_err| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    Ok("db's reset".to_string())
+}
+
+#[derive(serde::Deserialize, Debug)]
+struct Order {
+    id: i64,
+    region_id: i64,
+    gift_name: String,
+    quantity: i64,
+}
+
+async fn orders(
+    State(state): State<DbState>,
+    Json(orders): Json<Vec<Order>>,
+) -> Result<String, StatusCode> {
+    info!("13 orders started");
+    let sql = "INSERT INTO orders (id, region_id, gift_name, quantity) VALUES ($1, $2, $3, $4)";
+    for order in orders {
+        sqlx::query(sql)
+            .bind(order.id)
+            .bind(order.region_id)
+            .bind(order.gift_name)
+            .bind(order.quantity)
+            .execute(&state.pool)
+            .await
+            .map_err(|err| {
+                dbg!(&err);
+                StatusCode::INTERNAL_SERVER_ERROR
+            })?;
+    }
+
+    Ok("OK".to_string())
+}
+
+async fn orders_total(State(state): State<DbState>) -> Result<String, StatusCode> {
+    info!("13 orders total started");
+    let sql = "SELECT SUM(quantity) FROM orders";
+    let row: (i64,) = sqlx::query_as(sql)
+        .fetch_one(&state.pool)
+        .await
+        .map_err(|err| {
+            dbg!(&err);
+            StatusCode::INTERNAL_SERVER_ERROR
+        })?;
+
+    Ok(format!("{{\"total\": {}}}", row.0.to_string()))
+}
+
+async fn orders_popular(State(state): State<DbState>) -> Result<String, StatusCode> {
+    info!("13 orders popular started");
+    let sql = "SELECT gift_name FROM orders GROUP BY gift_name ORDER BY SUM(quantity) DESC LIMIT 1";
+    match sqlx::query_as::<_, (String,)>(sql)
+        .fetch_one(&state.pool)
+        .await
+    {
+        Ok(row) => Ok(format!("{{\"popular\": \"{}\"}}", row.0.to_string())),
+        Err(err) => match err {
+            sqlx::Error::RowNotFound => Ok("{\"popular\": null}".to_string()),
+            err => {
+                dbg!(&err);
+                Err(StatusCode::INTERNAL_SERVER_ERROR)
+            }
+        },
+    }
+}
+
 #[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
+async fn axum(
+    #[shuttle_shared_db::Postgres(
+        local_uri = "postgres://postgres:{secrets.PASSWORD}@localhost:5432/cch23"
+    )]
+    pool: PgPool,
+) -> shuttle_axum::ShuttleAxum {
     let shared_state = Arc::new(AppState {
         data: Arc::new(Mutex::new(HashMap::new())),
     });
+
+    sqlx::migrate!()
+        .run(&pool)
+        .await
+        .map_err(|err| shuttle_runtime::Error::Database(err.to_string()))?;
+
+    let db_state = DbState { pool };
+
     let router = Router::new()
         .route("/", get(ok))
         .route("/-1/error", get(error))
@@ -370,7 +486,13 @@ async fn main() -> shuttle_axum::ShuttleAxum {
         .route("/12/load/:string", get(load_string))
         .with_state(shared_state)
         .route("/12/ulids", post(ulids))
-        .route("/12/ulids/:weekday", post(ulids_weekday));
+        .route("/12/ulids/:weekday", post(ulids_weekday))
+        .route("/13/sql", get(sql))
+        .route("/13/reset", post(reset))
+        .route("/13/orders", post(orders))
+        .route("/13/orders/total", get(orders_total))
+        .route("/13/orders/popular", get(orders_popular))
+        .with_state(db_state);
 
     Ok(router.into())
 }
