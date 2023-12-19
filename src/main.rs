ab3f0ef4bbede95781727fedb965cfac6fc145e8
German Khokhlov <ouchlock@gmail.com>
2023-12-19 20:08:23 +0200

    14

--- src/main.rs
+++ src/main.rs
@@ -451,6 +451,47 @@ async fn orders_popular(State(state): State<DbState>) -> Result<String, StatusCo
     }
 }
 
+#[derive(serde::Deserialize, Debug, Clone, Default)]
+struct HtmlContent {
+    content: String,
+}
+
+async fn html_unsafe(Json(body): Json<HtmlContent>) -> Result<String, StatusCode> {
+    info!("14 html unsafe started");
+
+    Ok(format!(
+        "\
+<html>
+  <head>
+    <title>CCH23 Day 14</title>
+  </head>
+  <body>
+    {}
+  </body>
+</html>",
+        body.content
+    )
+    .to_string())
+}
+
+async fn html_safe(Json(body): Json<HtmlContent>) -> Result<String, StatusCode> {
+    info!("14 html safe started");
+
+    Ok(format!(
+        "\
+<html>
+  <head>
+    <title>CCH23 Day 14</title>
+  </head>
+  <body>
+    {}
+  </body>
+</html>",
+        html_escape::encode_double_quoted_attribute(body.content.as_str())
+    )
+    .to_string())
+}
+
 #[shuttle_runtime::main]
 async fn axum(
     #[shuttle_shared_db::Postgres(
@@ -492,7 +533,9 @@ async fn axum(
         .route("/13/orders", post(orders))
         .route("/13/orders/total", get(orders_total))
         .route("/13/orders/popular", get(orders_popular))
-        .with_state(db_state);
+        .with_state(db_state)
+        .route("/14/unsafe", post(html_unsafe))
+        .route("/14/safe", post(html_safe));
 
     Ok(router.into())
 }
